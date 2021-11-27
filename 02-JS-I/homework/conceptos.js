e295c786dd9697c8b66b6283606601339c53e0cf
oscar <oscarmercados75@gmail.com>
2021-11-26 21:32:21 -0500

    conceptos

---
+++ 02-JS-I/homework/conceptos.js
@@ -0,0 +1,13 @@
+ variables : Es un mecanismo para guardar los valores de varios datos para poder utilizarlos a futuro.
+
+ string : Es cualquier tipo de palabra, pero para aparezaca como una palabra debe estar encerrado en comillas ("").
+
+ funciones: Una funcion es una orden que le damos a nuestra maquina para que realice una accion;
+ Argumentos: son los datos que le suministramos a la funcion, si ingresamos o modificamos dichos datos sean numeros, letras, operaciones, etc.
+ Return: es el paso final a la funcion, es la orden que le damos a la maquina para que retorne los datos que suministramos en dicha funcion, el resultado de una suma, un nombre, etc.
+
+ Declaraciones if:  estas declaracione las usamos para la poner condiciones a la maquina para que algo se ejecute solo si se cumple la condicion dada.
+
+ Valores booleanos: estos valores almacenan unicamente dos tipos de datos, true(si es verdadero) y false(si es falso).
+
+ 
